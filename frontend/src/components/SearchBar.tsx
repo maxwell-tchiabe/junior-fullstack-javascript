fbca77d4b926f69207720a05eb3553d4f0e0f032
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2023-11-27 17:09:52 +0100

    add searchBar Component

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+import "./SearchBar.css";
+
+export const SearchBar = () => {
+  
+
+ 
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search..."
+      />
+    </div>
+  );
+};
