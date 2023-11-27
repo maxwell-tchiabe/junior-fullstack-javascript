a574258945a19c259e3d511ebf9cd53c51359507
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2023-11-27 17:52:35 +0100

    API call using fetch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(json());
 
 // Enable CORS middleware
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5003');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -4,6 +4,28 @@ import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
 export const SearchBar = () => {
+    const [input, setInput] = useState("");
+
+    const fetchData = (value: any) => {
+      fetch("http://localhost:5003/api/cities")
+        .then((response) => response.json())
+        .then((json) => {
+          const results = json.filter((city: any) => {
+            return (
+              value &&
+              city &&
+              city.cityname &&
+              city.cityname.toLowerCase().includes(value)
+            );
+          });
+          console.log(results);
+        });
+    };
+  
+    const handleChange = (value: any) => {
+      setInput(value);
+      fetchData(value);
+    };
   
 
  
@@ -12,6 +34,8 @@ export const SearchBar = () => {
       <FaSearch id="search-icon" />
       <input
         placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
       />
     </div>
   );
