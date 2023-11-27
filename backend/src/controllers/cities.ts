f8fdc9c4c08b9a0262d4c51c916d662f40cefbc0
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2023-11-27 01:31:29 +0100

    add getCitiesById Endpoint

--- backend/src/controllers/cities.ts
+++ backend/src/controllers/cities.ts
@@ -1,7 +1,7 @@
 import {pool} from '../database/database';
 import express, {json} from 'express';
 
-// Controller funcktion to get all appointments from the database
+// Controller funcktion to get all cities from the database
 async function all_cities_controller(req: express.Request, res: express.Response) {
     try {
       const query = 'SELECT cityId, cityName, cityCount FROM public.citiesTable'
@@ -12,4 +12,23 @@ async function all_cities_controller(req: express.Request, res: express.Response
       return res.status(500).json({message: 'Server error'});
     }
   }
-  export {all_cities_controller}
+
+   // Controller funcktion to get  city  by its unique ID from the database
+   async function city_by_id_controller(req: express.Request, res: express.Response) {
+    try {
+      const id = req.params.id;
+      const query = 'SELECT citiesTable.cityId,citiesTable.cityName, citiesTable.cityCount FROM public.citiesTable where citiesTable.cityId = $1'
+      const result = await pool.query(query,[id]);
+
+      if(result.rows.length === 0){
+        console.log('city does not exist');
+        return res.status(401).json({message:'city does not exist'})
+    }
+      return res.status(200).json(result.rows);
+    } catch (error) {
+      console.log('Error during execution:', error.message );
+      res.status(500).json({message: 'Server error'});
+    }
+  }
+
+  export {all_cities_controller,city_by_id_controller}
--- backend/src/routes/cities_routes.ts
+++ backend/src/routes/cities_routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 //import {correct_token} from "../middleware/authorization.js"
-import {all_cities_controller} from "../controllers/cities"
+import {all_cities_controller, city_by_id_controller} from "../controllers/cities"
 
 
 const router = express.Router();
@@ -9,5 +9,7 @@ const router = express.Router();
 router.get('/',  all_cities_controller );
 
 
+/* GET  city by Id */
+router.get('/:id', city_by_id_controller );
 
   export default router;
